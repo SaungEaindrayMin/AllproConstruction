1b09b307ab80bbf2c47e7e6a80ce66a8492fd9bb
Loon Htet <loonhtet.dev@gmail.com>
2025-11-20 19:31:10 +0700

    [ui] improve

--- src/components/Layout/CallToAction.jsx
+++ src/components/Layout/CallToAction.jsx
@@ -1,41 +1,45 @@
-"use client"
+"use client";
 
 import { ArrowRight } from "lucide-react";
 import { Button } from "../ui/button";
 import { useMessages } from "@/providers/I18nProvider";
+import Link from "next/link";
 
 export const CallToAction = () => {
-  const { messages } = useMessages();
-  const t = messages.callToAction;
-  return (
-    <section className="max-w-7xl mx-auto pb-24 px-4 sm:px-6 lg:px-8">
-      <div className="w-full text-center space-y-10">
-        <div>
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
-            <span className="bg-text-primary-gradient text-transparent bg-clip-text">
-              {t.connect}
-            </span>{" "}
-            {t.firstTitle}
-          </h1>
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-            {t.secondTitle}
-          </h1>
-        </div>
+     const { messages } = useMessages();
+     const t = messages.callToAction;
+     return (
+          <section className="max-w-7xl mx-auto pb-24 px-4 sm:px-6 lg:px-8">
+               <div className="w-full text-center space-y-10">
+                    <div>
+                         <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
+                              <span className="bg-text-primary-gradient text-transparent bg-clip-text">
+                                   {t.connect}
+                              </span>{" "}
+                              {t.firstTitle}
+                         </h1>
+                         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                              {t.secondTitle}
+                         </h1>
+                    </div>
 
-        <div className="text-lg text-muted-foreground w-full md:w-[50%] lg:w-[35%] mx-auto">
-          <p>{t.text}</p>
-        </div>
+                    <div className="text-lg text-muted-foreground w-full md:w-[50%] lg:w-[35%] mx-auto">
+                         <p>{t.text}</p>
+                    </div>
 
-        <div className="flex justify-center">
-          <div className="flex items-center justify-between gap-x-3 md:w-[35%] lg:w-[25%] bg-muted-foreground/10 p-3 rounded-full">
-            <p className="text-lg font-medium text-primary">{t.button}</p>
-
-            <Button className="p-2 bg-primary rounded-full">
-              <ArrowRight />
-            </Button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+                    <Link href="/contact" className="cursor-pointer">
+                         <div className="flex justify-center">
+                              <div className="group flex items-center justify-between gap-x-3 md:w-[35%] lg:w-[25%] hover:bg-primary bg-muted-foreground/10 p-3 rounded-full transition-colors duration-300">
+                                   <p className="text-lg font-medium text-primary ps-1 group-hover:text-white">
+                                        {t.button}
+                                   </p>
+                                   <Button className="p-2 bg-primary rounded-full group-hover:bg-white border-none">
+                                        <ArrowRight className="text-white group-hover:text-primary" />
+                                   </Button>
+                              </div>
+                         </div>
+                    </Link>
+               </div>
+          </section>
+     );
 };
--- src/components/Projects/ProjectGallery.jsx
+++ src/components/Projects/ProjectGallery.jsx
@@ -4,75 +4,76 @@ import { Button } from "../ui/button";
 import image1 from "../../../public/images/Hero.png";
 import image2 from "../../../public/images/Project1.jpg";
 import image3 from "../../../public/images/Project2.jpg";
-import image4 from "../../../public/images/Service2.jpg";
-import image5 from "../../../public/images/Service1.jpg";
-import image6 from "../../../public/images/WhoWeAre.jpg";
-import image7 from "../../../public/images/WhyUs.jpg";
+import image4 from "../../../public/images/Service2.png";
+import image5 from "../../../public/images/Service1.png";
+import image6 from "../../../public/images/WhoWeAre.png";
+import image7 from "../../../public/images/WhyUs.png";
 import image8 from "../../../public/images/Hero.png";
 import image9 from "../../../public/images/Project1.jpg";
 import image10 from "../../../public/images/Project2.jpg";
-import image11 from "../../../public/images/Service2.jpg";
-import image12 from "../../../public/images/Service1.jpg";
+import image11 from "../../../public/images/Service2.png";
+import image12 from "../../../public/images/Service1.png";
 
 const images = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
+     image1,
+     image2,
+     image3,
+     image4,
+     image5,
+     image6,
+     image7,
+     image8,
+     image9,
+     image10,
+     image11,
+     image12,
 ];
 
 export const ProjectGallery = () => {
-  return (
-    <section  className="w-full py-12 ">
-      <div>
-        <div className="text-center mb-10">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-            <span className="bg-text-primary-gradient text-transparent bg-clip-text">
-              Project{" "}
-            </span>
-            Gallery
-          </h1>
-          <p className="text-muted-foreground/70 text-sm md:text-base">
-            Discover what clients were able to achieve with Block.
-          </p>
-        </div>
+     return (
+          <section className="w-full py-20 ">
+               <div>
+                    <div className="text-center mb-10">
+                         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+                              <span className="bg-text-primary-gradient text-transparent bg-clip-text">
+                                   Project{" "}
+                              </span>
+                              Gallery
+                         </h1>
+                         <p className="text-muted-foreground/70 text-sm md:text-base">
+                              Discover what clients were able to achieve with
+                              Block.
+                         </p>
+                    </div>
 
-        <div className="flex flex-wrap justify-center gap-2">
-          {images.map((src, i) => (
-            <div
-              key={i}
-              className="
-                h-24          
-                sm:h-32        
-                md:h-36       
-                lg:h-40 
+                    <div className="flex flex-wrap justify-center gap-2">
+                         {images.map((src, i) => (
+                              <div
+                                   key={i}
+                                   className="
+                h-24
+                sm:h-32
+                md:h-36
+                lg:h-40
                 w-auto overflow-hidden rounded-sm
               "
-            >
-              <Image
-                src={src}
-                alt="project image"
-                className="h-full w-auto object-cover rounded-xs"
-              />
-            </div>
-          ))}
-        </div>
+                              >
+                                   <Image
+                                        src={src}
+                                        alt="project image"
+                                        className="h-full w-auto object-cover rounded-xs"
+                                   />
+                              </div>
+                         ))}
+                    </div>
 
-        {/* button */}
-        <div className="flex justify-center mt-10">
-          <Button variant="default" className="font-light">
-            VIEW PROJECT GALLERY
-          </Button>
-        </div>
-      </div>
-    </section>
-  );
+                    {/* button */}
+                    <div className="flex justify-center mt-10">
+                         <Button variant="default" className="font-light">
+                              VIEW PROJECT GALLERY
+                         </Button>
+                    </div>
+               </div>
+          </section>
+     );
 };
