9a11e0315730fec9b91a654a1d5b0c7dfa0a3c0d
Loon Htet <loonhtet.dev@gmail.com>
2025-11-20 19:59:55 +0700

    [update] contact us

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,5 +1,6 @@
 import { CallToAction } from "@/components/Layout/CallToAction";
 import { I18nProvider } from "@/providers/I18nProvider";
+import NextTopLoader from "nextjs-toploader";
 
 async function getMessages(locale) {
      const messages = await import(`../../../public/locales/${locale}.json`);
@@ -12,6 +13,7 @@ export default async function LocaleLayout({ children, params }) {
 
      return (
           <I18nProvider locale={locale} messages={messages}>
+               <NextTopLoader color="#1b4050" showSpinner={false} />
                {children}
           </I18nProvider>
      );
