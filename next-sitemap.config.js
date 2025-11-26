0a1ec370c9bec4e554341e43675c831f36dad293
Saung Eaindray Min <saungeaindraymin@gmail.com>
2025-11-26 10:17:28 +0630

    [FIX] site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  //   siteUrl: process.env.SITE_URL || 'https://example.com',
-  siteUrl: "https://allpro-construction.vercel.app/en",
-
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: "https://allpro-construction.vercel.app",
+  generateRobotsTxt: true,
 };
